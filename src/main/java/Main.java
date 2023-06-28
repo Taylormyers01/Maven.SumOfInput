66b4facfe8eb5366b8074ec00f40a9da7f6cb3b6
Taylor <taylor@Zipcoders-MacBook-Pro.local>
2023-06-28 19:41:18 -0400

    updated main to do all actions requested

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -8,19 +10,30 @@ public class Main {
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
+
+        int sum2 = gaussianSumOfNumbers(number); 
         
         System.out.println(sum);
+        System.out.println(sum2);
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        long start = System.currentTimeMillis();
+        int hold = 0;
+        for(int i =0; i <= n; i++){
+            hold = hold + i;
+        }
+        long end =  System.currentTimeMillis() - start; 
+        System.out.format("Loop took %d milliseconds to run\n", end);
+
+        return hold;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -35,11 +48,21 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        long start = System.currentTimeMillis();
+        n = n*(n+1)/2;
+        long end =  System.currentTimeMillis() - start;
+        System.out.format("Gaussian sum took %d milliseconds to run\n", end);
+        return n;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+
+    //Enter any number: 100000000
+    //Loop start time1687994590733
+    //Loop end time1687994590802
+    //Gauss start time1687994590802
+    //Gauss end time1687994590802
 }
